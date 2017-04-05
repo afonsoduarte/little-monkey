bd216bcf623e0f36d42152fe2ae23e61bf7ec208
Afonso Duarte <afonsomartins@gmail.com>
2017-04-05 16:34:48 +0100

    updates webpack config

--- tools/webpack.config.js
+++ tools/webpack.config.js
@@ -1,7 +1,23 @@
-var path = require('path');
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   entry: './app/scripts/app.js',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          //resolve-url-loader may be chained before sass-loader if necessary
+          use: 'css-loader'
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("main.css"),
+  ]
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, '../build/scripts'),
